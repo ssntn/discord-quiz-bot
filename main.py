9b8640238296c0260d8d9409710025cb02e67d76
iansisante <iansisante@gmail.com>
2022-09-10 17:19:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-import discord as disc
+import discord
 import commands
 
-TOKEN = "MTAxNjY0NzgwNjQxODY5NDIyNg.GrFx4O.bZLp1QNOU0koRZ38aCeuEzAm4rNzJJs53m0jIo"
-
-
-# mode = input("wala lang")
-# print(mode)
-
-print(commands.random_command())
-
-
-# user_in = int(input("Your answer(1-4): "))
-# if(cmd_arr[rand][1] == ch[user_in-1]):
-#     print("TAMA KA")
-# else:
-#     print("MALI KA!!!")
+question = commands.random_command()
+print("\n\nQuestion",question,"\n")
+print("Choice: ", commands.choices(question[1]))
